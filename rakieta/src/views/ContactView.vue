5f7f41dc9e1c84c184eb77be796d5a4b5895024d
Kekto <lekto8814@gmail.com>
2024-03-10 18:47:56 +0100

    Contacts

--- rakieta/src/views/ContactView.vue
+++ rakieta/src/views/ContactView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Card from 'primevue/Card';
 import Image from 'primevue/image';
+import Divider from 'primevue/divider';
 </script>
 
 <template>
@@ -9,25 +10,25 @@ import Image from 'primevue/image';
     <template #content>
       <div class="row" style="display: flex; flex-direction: row; gap: 16px;">
         <div class="names" style="width: calc(100% - 400px); display: flex; flex-wrap: wrap; align-content: flex-start;">
+          <h3 style="padding-top: 12px;">Address</h3>
           <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
             quas!
           </p>
+          <h3 style="padding-top: 12px;">Contact 1</h3>
           <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
             quas!
           </p>
+          <h3 style="padding-top: 12px;">Contact 2</h3>
           <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
             quas!
           </p>
         </div>
-        <Image src="https://i.pinimg.com/736x/31/e2/a1/31e2a17fc75dd7d332086446d8ebb40d.jpg" alt="Image" width="400px" preview />
+        <Divider layout="vertical"/>
+        <Image src="https://i.pinimg.com/736x/31/e2/a1/31e2a17fc75dd7d332086446d8ebb40d.jpg" alt="Image" width="400px" style="height:fit-content; align-self: center;" preview />
       </div>
-      <p class="m-0">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-        quas!
-      </p>
     </template>
   </Card>
 </template>
